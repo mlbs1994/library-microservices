9c5569e8c910ffe196d98df7c7867104701a8bfd
mlbs1994 <matheuslevibarouh@gmail.com>
2024-08-31 16:30:54 -0300

    Initializing loanservice-api project

---
+++ loanservice-api/src/main/java/com/mlbs/loanservice_api/LoanserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.mlbs.loanservice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanserviceApiApplication.class, args);
+	}
+
+}
---
+++ loanservice-api/src/test/java/com/mlbs/loanservice_api/LoanserviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlbs.loanservice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanserviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
