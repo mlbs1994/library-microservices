88d58f88dd795b1dcc2ee5c8fd98fc5b658c29f5
mlbs1994 <matheuslevibarouh@gmail.com>
2024-09-02 19:55:37 -0300

    Adding GET list method in BookController.java

--- books-api/src/main/java/com/mlbs/booksapi/controllers/BookController.java
+++ books-api/src/main/java/com/mlbs/booksapi/controllers/BookController.java
@@ -1,6 +1,9 @@
 package com.mlbs.booksapi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +20,15 @@ public class BookController {
 	@Autowired
 	BookService service;
 	
+	@GetMapping
+	public ResponseEntity<Page<ReadBookDTO>> list(@PageableDefault(size = 5, sort = "title") Pageable pagination) {
+		return ResponseEntity.ok(service.list(pagination));
+	}
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<ReadBookDTO> get(@PathVariable Integer id) {
 		return ResponseEntity.ok(service.read(id)); 
 	}
+	
 
 }
