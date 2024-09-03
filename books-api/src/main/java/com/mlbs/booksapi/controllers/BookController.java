4ee3746c7816cd5ffd8bc92aa056ea3e3126deed
mtlvbhsantos <mtlvbhsantos@gmail.com>
2024-09-03 13:32:12 -0300

    Adding DELETE method delete book

--- books-api/src/main/java/com/mlbs/booksapi/controllers/BookController.java
+++ books-api/src/main/java/com/mlbs/booksapi/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,12 @@ public class BookController {
 	public ResponseEntity<ReadBookDTO> update(@RequestBody UpdateBookDTO data) {
 		ReadBookDTO updatedBookDTO = service.update(data);
 		return ResponseEntity.ok(updatedBookDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 
